2d172f6c8b4c96b9596c2755b7b117088d0458d9
ridgekit <103030025+ridgekit@users.noreply.github.com>
2022-04-06 16:38:52 +0800

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,33 @@
 import React from "react"
+import axios from "axios"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = (catalogs) => {
+
+  var config = {
+      method: 'get',
+      url: 'https://product.gelatoapis.com/v3/catalogs',
+      headers: {
+        'X-API-KEY': process.env.API_KEY
+      },
+      data: {
+        ...catalogs
+      }
+  }
+  console.log ("CONFIG " + axios.config)
+  axios(config)
+  .then(function (response){
+    console.log(JSON.stringify(response.data))
+  })
+  .catch(function (error) {
+    console.log(error)
+  });
+
+  return (
+    <div>
+        <h1>Home Page</h1>
+
+    </div>
+  )
 }
+
+export default IndexPage
