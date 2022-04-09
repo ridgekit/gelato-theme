b8ed5764cd7f1ac04d7ae83e171dc6cef92f64b1
ridgekit <103030025+ridgekit@users.noreply.github.com>
2022-04-09 23:34:15 +0800

    Updated axios

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,28 +3,30 @@ import axios from "axios"
 
 const IndexPage = (catalogs) => {
 
-  // var config = {
-  //     method: 'get',
-  //     url: 'https://product.gelatoapis.com/v3/catalogs',
-  //     headers: {
-  //       'X-API-KEY': process.env.API_KEY,
-  //       'Access-Control-Allow-Origin': '*',
-  //       'X-Requested-With': 'XMLHttpRequest'
-  //     },
-  //     data: {
-  //       ...catalogs
-  //     }
-  // }
-  // console.log ("CONFIG " + JSON.stringify(config))
-  // axios(config)
-  // .then(function (response){
-  //   console.log(JSON.stringify(response.data))
-  //   let data = response.data
-  //   return data
-  // })
-  // .catch(function (error) {
-  //   console.log(error)
-  // });
+  var config = {
+      method: 'get',
+      url: 'https://product.gelatoapis.com/v3/catalogs',
+      headers: {
+        'X-API-KEY': process.env.API_KEY,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json'
+        // 'Access-Control-Allow-Origin': '*',
+        // 'X-Requested-With': 'XMLHttpRequest'
+      },
+      data: {
+        ...catalogs
+      }
+  }
+  console.log ("CONFIG " + JSON.stringify(config))
+  axios(config)
+  .then(function (response){
+    console.log(JSON.stringify(response.data))
+    let data = response.data
+    return data
+  })
+  .catch(function (error) {
+    console.log(error.toJSON())
+  });
 
   // const res = axios.get('https://product.gelatoapis.com/v3/catalogs', {
   //   headers: {
@@ -34,20 +36,20 @@ const IndexPage = (catalogs) => {
   // })
 
 
-  const url = 'https://product.gelatoapis.com/v3/catalogs'
+  // const url = 'https://product.gelatoapis.com/v3/catalogs'
 
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-API-KEY': process.env.API_KEY,
-    }
-  }
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-API-KEY': process.env.API_KEY,
+  //   }
+  // }
 
-  fetch(url, options)
-  .then((response) => response.json())
-  .then((data) => {
-      console.log(data)
-  });
+  // fetch(url, options)
+  // .then((response) => response.json())
+  // .then((data) => {
+  //     console.log(data)
+  // });
 
   return (
     <div>
